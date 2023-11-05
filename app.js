a3be8de2b4ad20b6c1709b0e70f8984a30b7d05b
Arien <Laurilin2022@gmail.com>
2023-11-05 14:07:53 +0100

    Style Main Content

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const BUTTON = document.querySelector('button')
+const BUTTON = document.querySelector('.like__button')
 
 const LIKE = () => {
   BUTTON.setAttribute('aria-pressed', BUTTON.matches('[aria-pressed=false]') ? true : false)
 }
 
 BUTTON.addEventListener('click', LIKE)
+/* email*/
+const FORM = document.querySelector('.form__email')
+FORM.addEventListener('submit', event => event.preventDefault())
+
+
